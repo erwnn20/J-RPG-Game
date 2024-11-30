6917ebb8bb1ed3552f9b82ea4fd16046524b2571
verwann <erwann.varlet@ynov.com>
2024-12-01 00:50:45 +0100

    implement SpecialAbility.Execute

--- Characters/Skills/SpecialAbility.cs
+++ Characters/Skills/SpecialAbility.cs
@@ -47,6 +47,80 @@ public class SpecialAbility<TTarget>(
 
     public override void Execute()
     {
-        throw new NotImplementedException();
+        if (!IsTargetCorrect())
+        {
+            Console.WriteLine(Target != null
+                ? $"La cible sélectionnée ({Target.Name} - {Target.GetType().Name}) ne correspond pas au type de cible de la compétence ({TargetType})."
+                : "Pas de cible sélectionné.");
+
+            return;
+        }
+
+        switch (Target)
+        {
+            case Character character:
+                Execute(character);
+                break;
+            case Team.Team team:
+                Execute(team);
+                break;
+            default:
+                Console.WriteLine(Target != null
+                    ? $"Erreur de type de cible sur {Name}. Attendu: {nameof(Character)} ou {nameof(Team.Team)}, Actuel: {Target.GetType().Name}."
+                    : $"Erreur de cible sur {Name}. La cible est null.");
+                break;
+        }
+
+        Target = null;
+    }
+
+    private void Execute(Character _, Func<Character, string>? message = null)
+    {
+        if (Target is not Character target || this is not SpecialAbility<Character>)
+        {
+            Console.WriteLine(Target != null
+                ? $"Erreur de cible sur {Name}. Attendu: {nameof(Character)}, Actuel: {Target.GetType().Name}.)"
+                : $"Erreur de cible sur {Name}. La cible est null.");
+            return;
+        }
+
+        if (!target.IsAlive(false))
+        {
+            Console.WriteLine($"La cible de {Name} est morte, la capacité ne peux donc pas être utilisée");
+            return;
+        }
+
+        message ??= t => $"{Owner.Name} utilise {Name}" +
+                         (TargetType != TargetType.Self ? $" sur {t.Name}" : string.Empty);
+        Console.WriteLine(message(target));
+        Console.WriteLine(Effect(target));
+    }
+
+    private void Execute(Team.Team _)
+    {
+        if (Target is not Team.Team team)
+        {
+            Console.WriteLine(Target != null
+                ? $"Erreur de cible sur {Name}. Attendu: {nameof(Team.Team)}, Actuel: {Target.GetType().Name}.)"
+                : $"Erreur de cible sur {Name}. La cible est null.");
+            return;
+        }
+
+        Console.WriteLine($"{Owner.Name} fait {Name} sur l'équipe {Target.Name}");
+        team.Characters
+            .Where(c => c.IsAlive(false)).ToList()
+            .ForEach(target =>
+            {
+                new SpecialAbility<Character>(
+                    name: Name,
+                    description: Description,
+                    owner: Owner,
+                    target: target,
+                    targetType: TargetType.Other,
+                    reloadTime: 0,
+                    manaCost: 0,
+                    effect: Effect
+                ).Execute(target, t => $"{Name} atteint {t.Name}");
+            });
     }
 }
